4ee3cb1ceeb8480070d2f2f12e83ede160600ec3
Victor Nogueira <felladrin@gmail.com>
2021-07-18 01:50:50 +0300

    Memoise "toggleTheme" function, for performance

--- src/components/ToggleThemeButton.tsx
+++ src/components/ToggleThemeButton.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
 import { Theme } from "../enums/Theme";
 import { useLocalStorage } from "react-use";
@@ -6,12 +6,14 @@ import { useLocalStorage } from "react-use";
 export function ToggleThemeButton() {
   const [theme, setTheme] = useLocalStorage("data-theme", Theme.Dark);
 
+  const toggleTheme = useCallback(() => setTheme(theme === Theme.Dark ? Theme.Light : Theme.Dark), [theme]);
+
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme as Theme);
   }, [theme]);
 
   return (
-    <a onClick={() => setTheme(theme === Theme.Dark ? Theme.Light : Theme.Dark)} data-test-id="toggle-theme-button">
+    <a onClick={toggleTheme} data-test-id="toggle-theme-button">
       {theme === Theme.Dark && (
         <span title="Use Light Theme">
           <FaSun />
