d03bf84c44e875426f60e5baeb32cb4895e2759a
Victor Nogueira <felladrin@gmail.com>
2022-08-07 11:10:56 +0000

    Adjust icons from Intro Component

--- src/components/intro/index.ts
+++ src/components/intro/index.ts
@@ -1,4 +1,25 @@
 import { parseHtmlString } from "../../functions/parseHtmlString";
 import htmlString from "./index.html?raw";
+import {
+  faGithub,
+  faImdb,
+  faItchIo,
+  faLinkedin,
+  faStackExchange,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import { replaceHtmlSlots } from "../../functions/replaceHtmlSlots";
+import { getIconNode } from "../../functions/getIconNode";
 
 export const intro = parseHtmlString(htmlString).firstElementChild;
+
+replaceHtmlSlots(intro, {
+  faLinkedin: getIconNode(faLinkedin),
+  faGithub: getIconNode(faGithub),
+  faTwitter: getIconNode(faTwitter),
+  faStackExchange: getIconNode(faStackExchange),
+  faYoutube: getIconNode(faYoutube),
+  faItchIo: getIconNode(faItchIo),
+  faImdb: getIconNode(faImdb),
+});
--- src/components/toggleThemeButton/index.ts
+++ src/components/toggleThemeButton/index.ts
@@ -1,12 +1,12 @@
 import halfmoon from "halfmoon";
-import { icon } from "@fortawesome/fontawesome-svg-core";
 import { faMoon } from "@fortawesome/free-solid-svg-icons";
 import { parseHtmlString } from "../../functions/parseHtmlString";
 import htmlString from "./index.html?raw";
 import { replaceHtmlSlots } from "../../functions/replaceHtmlSlots";
+import { getIconNode } from "../../functions/getIconNode";
 
 export const toggleThemeButton = parseHtmlString(htmlString).firstElementChild;
 
 toggleThemeButton.addEventListener("click", () => halfmoon.toggleDarkMode());
 
-replaceHtmlSlots(toggleThemeButton, { moonIcon: icon(faMoon).node.item(0) });
+replaceHtmlSlots(toggleThemeButton, { moonIcon: getIconNode(faMoon) });
