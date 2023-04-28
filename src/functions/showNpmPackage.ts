654d5877e2aa5d2333153b5908499ffcf219d1fd
Victor Nogueira <felladrin@gmail.com>
2023-04-28 11:40:33 +0300

    Update the size of WinBox opened when clicking an NPM package name

--- src/functions/showNpmPackage.ts
+++ src/functions/showNpmPackage.ts
@@ -6,12 +6,10 @@ export function showNpmPackage(packageName: string) {
     url: `https://yarn.pm/${packageName}#readme`,
     x: "center",
     y: "center",
-    width: "60%",
-    height: "60%",
+    width: "90%",
+    height: "80%",
     border: "0.3em",
     background: "#117cad",
-    top: 50,
-    max: true,
     class: ["no-min", "no-full"],
   });
 }
